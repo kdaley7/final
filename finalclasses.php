cc9eec77ff9cf24ab5daf06f9b0d14cd92807db3
kdaley7 <90045746+kdaley7@users.noreply.github.com>
2021-12-17 19:49:11 -0500

    Add files via upload

---
+++ finalclasses.php
@@ -0,0 +1,26 @@
+<?php
+class companycars{
+    public $carid
+    public $carmake
+    public $model
+    public $year
+    public $licenceplate
+    }
+
+class crash {
+    public $employeeID
+    public $lastname
+    public $firstname
+    public $damage
+    public $accidentreport
+}
+
+class employeeid {
+    public $ID
+    public $firstname
+    public $lastname
+    public $address
+    public $city
+    public $state
+    public $carid
+}
